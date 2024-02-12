7c26b076613822d05537b74171f92fce6b44a9cd
Milan <milangru@gmail.com>
2024-02-12 22:08:57 +0100

    Added formatting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import pandas 
+import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -18,6 +18,7 @@ else:
 
 # FUNCTIONS
 
+
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -27,16 +28,19 @@ def next_card():
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(4000, func=flip_card)
 
+
 def flip_card():
-    canvas.itemconfig(card_title, text="English", fill= "white")
+    canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
 
 def is_known():
     to_learn.remove(current_card)
     data = pandas.DataFrame(to_learn)
     data.to_csv("data/words_to_learn.csv", index=False)
     next_card()
+
 
 # USER INTERFACE
 
